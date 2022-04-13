fc387fe478d604eaa58732d3417d2822de685065
BoostButt2 <55084098+BoostButt2@users.noreply.github.com>
2022-04-13 16:02:30 +0200

    Added rotation, panning and zoom
    Panning and zoom not working properly

---
+++ Assets/Scripts/MoveModel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Moved)
+            {
+                var rotation = new Vector3(0f, -touch0.deltaPosition.x, 0f);
+
+                GameObject.Find("Pivot").transform.Rotate(rotation * 0.1f);
+            }
+
+        }
+    }
+}
