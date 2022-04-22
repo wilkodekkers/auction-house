ba51d49d43a1bb4e6d22fcce5e6a80a233f7de80
Bramvzw <s1146510@student.windesheim.nl>
2022-04-22 10:21:00 +0200

    Upcoming Auctions
    -Button works

--- Assets/Scripts/MovetoScene.cs
+++ Assets/Scripts/MovetoScene.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MovetoScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ButtonMoveScene() 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("UpcomingAuction");
+        Debug.Log("Tapped");
     }
 }
