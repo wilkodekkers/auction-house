f2d683fee940a3a2f77da5d93c1c9ac60f934f1a
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-05-13 10:17:03 +0200

    Add list view

--- Assets/Scripts/AuctionService.cs
+++ Assets/Scripts/AuctionService.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -8,6 +9,8 @@ public class AuctionService : MonoBehaviour
 {
     private const string BASE_ENDPOINT = "https://localhost:7254/api/auctionhouse";
     private List<Auction> auctions = new List<Auction>();
+
+    [SerializeField] private GameObject buttonTemplate;
 
     private void Start()
     {
@@ -26,7 +29,14 @@ public class AuctionService : MonoBehaviour
             else
             {
                 auctions = JsonConvert.DeserializeObject<List<Auction>>(request.downloadHandler.text);
-                Debug.Log($"({auctions.Count}) Auctions fetched");
+                GameObject g;
+                for (int i = 0; i < auctions.Count; i++) {
+                    var auction = auctions[i];
+                    g = Instantiate(buttonTemplate, transform);
+                    g.name = $"item {i}";
+                    GameObject.Find($"{g.name}/title").GetComponent<TextMeshProUGUI>().text = "Auction";
+                    GameObject.Find($"{g.name}/description").GetComponent<TextMeshProUGUI>().text = $"{auction.startTime.ToString("dd-MM-yyyy")} \t\t\t {auction.startTime.ToString("HH:mm")} - {auction.endTime.ToString("HH:mm")}";
+                }
             }
         }
     }
