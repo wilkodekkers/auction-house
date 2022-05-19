fe9bd00b3763a432988effa9e39494b1ea680413
collinWindesheim <57794123+collinWindesheim@users.noreply.github.com>
2022-05-19 11:54:42 +0200

    Username in bid screen (#28)
    * Fix Newtonsoft error
    
    * Set bid screen to landscape
    
    * Add name display to bid screen
    
    * Fix loaded scene
    
    Deze wordt alleen gebruikt bij testing
    
    * Change loaded scene
    
    * Removed Newtonsoft Nuget package and dll
    
    * Fix project version
    
    * Update buildsettings
    
    * Fix file name change

---
+++ Assets/Scripts/RealTimeDatabase.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using Firebase.Database;
+using UnityEngine.UI;
+using TMPro;
+
+public class RealTimeDatabase : MonoBehaviour
+{
+    private float StartPrice;
+    private string winningPlayerName;
+
+    private Button priceButton;
+    private Button bidButton;
+    private Button winningPlayer;
+
+    DatabaseReference reference;
+
+    void Start()
+    {
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        bidButton = GameObject.Find("BidButton").GetComponent<Button>();
+        priceButton = GameObject.Find("PriceButton").GetComponent<Button>();
+        winningPlayer = GameObject.Find("winningPlayer").GetComponent<Button>();
+
+        bidButton.onClick.AddListener(PriceUp);
+        reference.Child("TestData").Child("data").Child("Bid").ValueChanged += HandleValuePriceChanged;
+        reference.Child("TestData").Child("data").Child("Email").ValueChanged += HandleNameValueChanged;
+    }
+
+    private void PriceUp()
+    {
+        winningPlayerName = PlayerInfo.email;
+
+        reference.Child("TestData").Child("data").Child("Bid").SetValueAsync(StartPrice + 1000);
+        reference.Child("TestData").Child("data").Child("Email").SetValueAsync(winningPlayerName);
+    }
+
+    private void HandleValuePriceChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+
+        StartPrice = float.Parse(args.Snapshot.Value.ToString());
+    }
+
+    private void HandleNameValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+
+        winningPlayerName = args.Snapshot.Value.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        priceButton.GetComponentInChildren<TextMeshProUGUI>().SetText(StartPrice.ToString());
+        winningPlayer.GetComponentInChildren<TextMeshProUGUI>().SetText(winningPlayerName);
+    }
+}
