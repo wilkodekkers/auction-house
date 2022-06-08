934843a8ee3e90728537aab0c68e4ae4e7cd3411
Jesse Huang <huangjesse3@gmail.com>
2022-06-08 11:10:14 +0200

    Optimalisation
    Code mooier gemaakt en geoptimaliseerd

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using PathCreation;
 
@@ -10,17 +8,14 @@ public class Follower : MonoBehaviour
     float distanceTravelled;
     public EndOfPathInstruction endOfPathInstruction;
 
-    // Start is called before the first frame update
     void Start()
     {
         speed = 44f / 30f;
     }
 
-    // Update is called once per frame
-    void Update()   
+    void Update()
     {
         distanceTravelled += speed * Time.deltaTime;
-        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+        transform.SetPositionAndRotation(pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction), pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction));
     }
 }
