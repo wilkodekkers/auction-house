615ff539f5381aeee2438ef9511438005acafc8c
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-06-09 17:08:35 +0200

    Fix game scene flow

--- Assets/Scripts/AuctionCountdownStart.cs
+++ Assets/Scripts/AuctionCountdownStart.cs
@@ -17,7 +17,7 @@ public class AuctionCountdownStart : MonoBehaviour
         auctionStartButton = GameObject.Find("ButtonAuctionStart").GetComponent<Button>();
         buttonText = auctionStartButton.GetComponentInChildren<TextMeshProUGUI>();
 
-        auctionStartButton.onClick.AddListener(() => SceneManager.LoadScene("bid_game"));
+        auctionStartButton.onClick.AddListener(() => SceneManager.LoadScene("bid_game_database_test"));
     }
 
     void Update()
