b574b170c97145b19b9b9780cbfa89dc808e2e56
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-06-09 16:53:25 +0200

    Add characters on stool

--- Assets/Scripts/RealTimeDatabase.cs
+++ Assets/Scripts/RealTimeDatabase.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 using Firebase.Database;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class RealTimeDatabase : MonoBehaviour
 {
     private float StartPrice;
     public string WinningPlayerName { get; set; }
-
+    private List<GameObject> characters = new List<GameObject>();
     private Button priceButton;
     private Button bidButton;
     private Button winningPlayer;
+
+    [SerializeField] private Transform objectToLookAt;
+    [SerializeField] private GameObject playerModel;
 
     DatabaseReference reference;
 
@@ -22,9 +26,25 @@ public class RealTimeDatabase : MonoBehaviour
         priceButton = GameObject.Find("PriceButton").GetComponent<Button>();
         winningPlayer = GameObject.Find("winningPlayer").GetComponent<Button>();
 
+        InitializeCharacters();
+
         bidButton.onClick.AddListener(PriceUp);
         reference.Child("TestData").Child("data").Child("Bid").ValueChanged += HandleValuePriceChanged;
         reference.Child("TestData").Child("data").Child("Email").ValueChanged += HandleNameValueChanged;
+    }
+
+    private void InitializeCharacters()
+    {
+        // Find all stools in the scene
+        var spawns = GameObject.FindGameObjectsWithTag("Stool");
+
+        foreach (var stool in spawns)
+        {
+            // Put a character on each stool
+            GameObject character = Instantiate(playerModel);
+            character.transform.position = stool.transform.position + new Vector3(0.0f, 0.6f, 0.0f);
+            characters.Add(character);
+        }
     }
 
     private void PriceUp()
@@ -61,5 +81,11 @@ public class RealTimeDatabase : MonoBehaviour
     {
         priceButton.GetComponentInChildren<TextMeshProUGUI>().SetText(StartPrice.ToString());
         winningPlayer.GetComponentInChildren<TextMeshProUGUI>().SetText(WinningPlayerName);
+
+        // Let all characters look at the moving object in the scene
+        foreach (var character in characters)
+        {
+            character.transform.LookAt(objectToLookAt);
+        }
     }
 }
