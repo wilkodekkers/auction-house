75dff1ee9bfbe3310988f5fb97f386362ad6e491
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-06-16 16:01:53 +0200

    Improved script performance

--- Assets/LoadModel.cs
+++ Assets/LoadModel.cs
@@ -3,36 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadModel : MonoBehaviour
-{
-    [SerializeField] private GameObject ModelParent;
-
+{   
     [SerializeField] private GameObject ContractModel;
     [SerializeField] private GameObject BottlingLineModel;
-    [SerializeField] private GameObject Press;
-    [SerializeField] private GameObject Fridge;
+    [SerializeField] private GameObject PressModel;
+    [SerializeField] private GameObject FridgeModel;
 
     void Start()
     {
-        var modelName = PlayerPrefs.GetString("model");
         GameObject go = null;
-        switch (modelName) {
+        switch (PlayerPrefs.GetString("model")) {
             case "contract":
-                go = Instantiate(ContractModel, ModelParent.transform.position, Quaternion.identity);
+                go = Instantiate(ContractModel, transform.position, Quaternion.identity);
                 break;
             case "bottlingline":
-                go = Instantiate(BottlingLineModel, ModelParent.transform.position, Quaternion.identity);
+                go = Instantiate(BottlingLineModel, transform.position, Quaternion.identity);
                 break;
             case "press":
-                go = Instantiate(Press, ModelParent.transform.position, Quaternion.identity);
+                go = Instantiate(PressModel, transform.position, Quaternion.identity);
                 break;
             case "fridge":
-                go = Instantiate(Fridge, ModelParent.transform.position, Quaternion.identity);
+                go = Instantiate(FridgeModel, transform.position, Quaternion.identity);
                 break;
             default:
+                go =  Instantiate(BottlingLineModel, transform.position, Quaternion.identity);
                 break;
         }
-        if (go != null) {
-            go.transform.SetParent(ModelParent.transform);
-        }
+        go.transform.SetParent(transform);
     }
 }
