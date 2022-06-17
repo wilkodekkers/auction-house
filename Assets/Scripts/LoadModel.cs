7e01a3b713c9c7ecae1cb7403722d18c44e53156
Wilko Dekkers <wilko.dekkers@gmail.com>
2022-06-17 14:24:59 +0200

    Fix contract and bottling line animatitons

--- Assets/Scripts/LoadModel.cs
+++ Assets/Scripts/LoadModel.cs
@@ -12,12 +12,30 @@ public class LoadModel : MonoBehaviour
         var Position = transform.position;
         var Go = PlayerPrefs.GetString("model") switch
         {
-            "contract" => Instantiate(contractModel, Position, Quaternion.identity),
-            "bottlingline" => Instantiate(bottlingLineModel, Position, Quaternion.identity),
+            "contract" => LoadContractModel(),
+            "bottlingline" => LoadBottlingLine(),
             "press" => Instantiate(pressModel, Position, Quaternion.identity),
             "fridge" => Instantiate(fridgeModel, Position, Quaternion.identity),
             _ => Instantiate(bottlingLineModel, Position, Quaternion.identity)
         };
         Go.transform.SetParent(transform);
+    }
+
+    private GameObject LoadContractModel()
+    {
+        var Model = Instantiate(contractModel, transform.position, Quaternion.identity);
+        Model.transform.localScale = new Vector3(.3f, .3f, .3f);
+        Model.transform.position += new Vector3(0f, .3f, 0f);
+
+        return Model;
+    }
+
+    private GameObject LoadBottlingLine()
+    {
+        var Model = Instantiate(bottlingLineModel, transform.position, Quaternion.identity);
+        Model.transform.localScale = new Vector3(.1f, .1f, .1f);
+        Model.transform.position += new Vector3(-2f, .3f, -4f);
+
+        return Model;
     }
 }
